b74b210fde5dd736e765f5b4c3d5d41cc59904ba
VioletViola <vivian.v18723@gmail.com>
2021-08-16 19:29:47 +0100

    Compose most of the number guessing game with the while loop.

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -1,4 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class NumberGuessingGame {
     public static void main(String[] args) {
+        boolean continueGame = true;
+
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        int computerGuess = rand.nextInt(5);
+        int userGuess = scan;
+
+        while (continueGame) {
+            if (userGuess > computerGuess){
+                System.out.println("Too High!");
+            } else if (userGuess < computerGuess) {
+                System.out.println("Too Low!");
+            } else {
+                System.out.println("Correct!");
+                continueGame = false;
+            }
+        }
     }
 }
