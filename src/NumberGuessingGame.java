d7644ee7079f5a0de619b2a54b9f547b2e4dda2b
VioletViola <vivian.v18723@gmail.com>
2021-08-16 20:21:05 +0100

    Add if condition to restrict eligible user entries.

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -16,15 +16,20 @@ public class NumberGuessingGame {
         // while loop for guesses
         boolean continueGame = true;
         while (continueGame) {
-            if (userGuess > computerGuess){
-                System.out.println("Too High!");
-                userGuess = scan.nextInt();
-            } else if (userGuess < computerGuess) {
-                System.out.println("Too Low!");
+            if (userGuess <= 0 || userGuess >= 201) {
+                System.out.println("Out of bounds! Guess between 1 and 200 (inclusive)!");
                 userGuess = scan.nextInt();
             } else {
-                System.out.println("Correct! Game Over!");
-                continueGame = false;
+                if (userGuess > computerGuess){
+                    System.out.println("Too High!");
+                    userGuess = scan.nextInt();
+                } else if (userGuess < computerGuess) {
+                    System.out.println("Too Low!");
+                    userGuess = scan.nextInt();
+                } else {
+                    System.out.println("Correct! Game Over!");
+                    continueGame = false;
+                }
             }
         }
     }
